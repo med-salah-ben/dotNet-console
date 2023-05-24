7d2c3eae769b27c667062578835013a4158f3302
med-salah-ben <med.salah.ben.amou@gmail.com>
2023-05-24 20:56:57 +0100

    dotnet console

---
+++ RandomNumberGame/jeu/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace NombreMystereGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int nombreMystere = random.Next(0, 101);
+            int nombreEssai = 0;
+            bool close = false;
+
+            Console.WriteLine("Bienvenue dans le jeu du Trié!");
+            Console.WriteLine("--------------------------------");
+
+            while (!close)
+            {
+                Console.Write("Entrez un nombre entre 0 et 100 : ");
+                string input = Console.ReadLine();
+
+                if (!int.TryParse(input, out int nombre))
+                {
+                    Console.WriteLine("Erreur : Veuillez entrer un nombre valide.");
+                    continue;
+                }
+
+                if (nombre < 0 || nombre > 100)
+                {
+                    Console.WriteLine("Erreur : Le nombre doit être entre 0 et 100.");
+                    continue;
+                }
+
+                nombreEssai++;
+
+                if (nombre == nombreMystere)
+                {
+                    Console.WriteLine($"Great, you get correct number in {nombreEssai} tentative !");
+                    close = true;
+                }
+                else if (nombre < nombreMystere)
+                {
+                    Console.WriteLine("Le nombre plus grand.");
+                }
+                else
+                {
+                    Console.WriteLine("Le nombre plus petit.");
+                }
+            }
+
+            Console.WriteLine("Great !");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TriangleSurfaceCalculator/test/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace TriangleSurfaceCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double baseTriangle, hauteurTriangle;
+            Console.WriteLine("Calcul de la surface d'un triangle");
+            Console.WriteLine("----------------------------------");
+            Console.Write("Entrez la base du triangle : ");
+            if (!double.TryParse(Console.ReadLine(), out baseTriangle))
+            {
+                Console.WriteLine("La valeur de la base n'est pas valide.");
+                return;
+            }
+            Console.Write("Entrez la hauteur du triangle : ");
+            if (!double.TryParse(Console.ReadLine(), out hauteurTriangle))
+            {
+                Console.WriteLine("La valeur de la hauteur n'est pas valide.");
+                return;
+            }
+
+            double surface = getSurFace(baseTriangle, hauteurTriangle);
+            Console.WriteLine($"La surface du triangle est : {surface}");
+
+            Console.ReadLine();
+        }
+
+        static double getSurFace(double baseTriangle, double hauteurTriangle)
+        {
+            return (baseTriangle * hauteurTriangle) / 2;
+        }
+    }
+}
